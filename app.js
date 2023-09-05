40e258e25d572771ed612aacd7afb58ee6faf6d5
Samur <107584285+Samur000@users.noreply.github.com>
2023-09-06 00:57:44 +0300

    test practic

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
-let slides = document.querySelectorAll('.slide')
+let slides = document.querySelectorAll( '.slide' );
 
-for (let slide of slides) {
-   slide.addEventListener('click', () => {
-      clearActiveClass()
+for ( let slide of slides ){
+	slide.addEventListener( 'click', () => {
+		clearActiveClass();
 
-      slide.classList.add('active')
-   })
+		slide.classList.add( 'active' );
+	} );
 }
 
-function clearActiveClass() {
-   slides.forEach((slide) => {
-      slide.classList.remove('active')
-   })
+function clearActiveClass(){
+	slides.forEach( ( slide ) => {
+		slide.classList.remove( 'active' );
+	} );
 }
